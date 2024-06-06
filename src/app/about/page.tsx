05a9af1e9143609e19294b2c51edd491e2bc90ab
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2024-06-06 18:24:56 +0100

    fix: adjust responsiveness on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,15 +5,23 @@ export default function AboutPage() {
   const { image, aboutHeading, aboutContent } = profile
   return (
     <main className="container flex flex-col">
-      <div className="flex items-start gap-5">
-        <div className='md:w-[30em] flex-shrink-0'>
-          <Image src={image} alt={aboutHeading} width={419} height={627} className='w-full h-auto object-cover' />
+      <div className="flex flex-col md:flex-row items-start gap-5">
+        <div className="w-full md:w-[30em] flex-shrink-0">
+          <Image
+            src={image}
+            alt={aboutHeading}
+            width={419}
+            height={627}
+            className="w-full h-auto object-cover"
+          />
         </div>
-        <div className='flex flex-col items-start gap-6'>
-          <h1 className='text-8xl font-semibold'>{aboutHeading}</h1>
-          <div className='space-y-3'>
+        <div className="flex flex-col items-start gap-6">
+          <h1 className="text-5xl md:text-8xl font-semibold">{aboutHeading}</h1>
+          <div className="space-y-3">
             {aboutContent.map((content, idx) => (
-              <p className='text-primary/70' key={`content_${idx}`}>{content}</p>
+              <p className="text-primary/70" key={`content_${idx}`}>
+                {content}
+              </p>
             ))}
           </div>
         </div>
